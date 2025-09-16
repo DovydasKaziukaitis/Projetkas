36773b574b47a6da075f35668b928b4a6e39a352
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-09-17 00:30:36 +0300

    Update 1bandym.cpp

--- 1bandym.cpp
+++ 1bandym.cpp
@@ -2,10 +2,11 @@
 #include <iomanip>
 #include <vector>
 
+double avg(const std::vector<int>& a) {
+    return a.empty() ? 0.0
+                     : (double)std::accumulate(a.begin(),a.end(),0) / a.size();
 int main(){
-  int m;
-  std::cout <<"Irasykite kiek norite studentu ivesti: ";
-  if (!(std::cin >> m || m<= 0) {
-      std::cerr <<"neteisingas skaicius";
-      return 1;
-  }
+  int m; std::cout <<"Irasykite kiek norite studentu ivesti: "; if (!(std::cin >> m)) return 0;
+  std::vector<std::string> vardai(m),pavardes(m);
+  std::vector<double> galut(m);
+

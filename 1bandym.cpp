e0138e657427860aee2008c4c086273a7a8368de
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-09-17 20:55:10 +0300

    Update 1bandym.cpp

--- 1bandym.cpp
+++ 1bandym.cpp
@@ -14,18 +14,20 @@ int main(int argc, char** argv){
       
       std::string line;
       std::getline(f, line);
+      while (std::getline(f, line)) {
+          if (line.empty()) continue;
+          std::istringstream in(line);
+          std::string v, p; if (!(in >> v >> p)) continue;
 
-  for (int i=0; i < m, ++i) {
-      std::cout << "Vardas Pavarde: ";
-      std::cin >> vardai[i] >> pavardes[i];
+          std::vector<int> marks; int x;
+          while (in >> x) marks.push_back(x);
+          if (marks.empty()) continue;
 
-      int n; std::cout << "Kiek ND? "; std::cin >> n;
-      std::vector<int> nd(n);
-      for(int j = 0l j<n; ++j) std::cin>>nd[j];
-
-      int egz; std::cout << "Egzaminas: "; std::cin >> egz;
-      galut[i] = 0.4*avg(nd) + 0.6*egz;
-}
+          int egz = marks.back(); marks.pop_back();
+          vardai.push_back(v); pavardes.push_back(p);
+          galut.push_back(0.4 * avg(marks) + 0.6 * egz);
+      }     
+} else {
 std::cout << std::left << std::setw(15) << "Pavarde"
           << std::setw(15) << "Vardas"
           << std::right << std::setw(16) << "Galutinis(Vid)"

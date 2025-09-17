9766743df07e95f5e18d93ca54e0c54766210a95
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-09-17 21:12:34 +0300

    Update 1bandym.cpp

--- 1bandym.cpp
+++ 1bandym.cpp
@@ -5,6 +5,7 @@
 double avg(const std::vector<int>& a) {
     return a.empty() ? 0.0
                      : zccumulate(a.begin(),a.end(), 0.0) / a.size();
+}
 int main(int argc, char** argv){
   std::vector<std::string> vardai, pavardes;
   std::vector<double> galut;
@@ -27,23 +28,23 @@ int main(int argc, char** argv){
           vardai.push_back(v); pavardes.push_back(p);
           galut.push_back(0.4 * avg(marks) + 0.6 * egz);
       }     
-} else {
-    int m; std::cout << "Kiek studentu? ";
-    if(!(std::cin>>m) || m<= 0) return 0;
+  } else {
+      int m; std::cout << "Kiek studentu? ";
+      if(!(std::cin>>m) || m<= 0) return 0;
 
-    vardai.resize(m);pavardes.resize(m);galut.resize(m);
-    for(int i=0; i<m; ++i) {
-       std::cout << "Vardas Pavarde: ";
-       std::cin >> vardai[i] >> pavardas[i];
+      vardai.resize(m);pavardes.resize(m);galut.resize(m);
+      for(int i=0; i<m; ++i) {
+         std::cout << "Vardas Pavarde: ";
+         std::cin >> vardai[i] >> pavardas[i];
 
-       int n;std::cout <<"Kiek ND? "; std::cin >> n;
-       std::vector<int> nd(n);
-       for(int j=0; j<n; ++j) std::cin >> nd[j];
+         int n;std::cout <<"Kiek ND? "; std::cin >> n;
+         std::vector<int> nd(n);
+         for(int j=0; j<n; ++j) std::cin >> nd[j];
 
-       int egz; std::cout <<"Egzaminas: "; std::cin >> egz;
-       galut[i] = 0.4 * avg(nd) + 0.6 *egz;
-    }
-}
+         int egz; std::cout <<"Egzaminas: "; std::cin >> egz;
+         galut[i] = 0.4 * avg(nd) + 0.6 *egz;
+      }
+  }
 std::cout << std::left << std::setw(15) << "Pavarde"
           << std::setw(15) << "Vardas"
           << std::right << std::setw(16) << "Galutinis(Vid)"

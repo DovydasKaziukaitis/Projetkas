a7af3a8a94f40544ff92cc44ac9222f0c05f6539
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-09-17 21:09:08 +0300

    Update 1bandym.cpp

--- 1bandym.cpp
+++ 1bandym.cpp
@@ -28,6 +28,22 @@ int main(int argc, char** argv){
           galut.push_back(0.4 * avg(marks) + 0.6 * egz);
       }     
 } else {
+    int m; std::cout << "Kiek studentu? ";
+    if(!(std::cin>>m) || m<= 0) return 0;
+
+    vardai.resize(m);pavardes.resize(m);galut.resize(m);
+    for(int i=0; i<m; ++i) {
+       std::cout << "Vardas Pavarde: ";
+       std::cin >> vardai[i] >> pavardas[i];
+
+       int n;std::cout <<"Kiek ND? "; std::cin >> n;
+       std::vector<int> nd(n);
+       for(int j=0; j<n; ++j) std::cin >> nd[j];
+
+       int egz; std::cout <<"Egzaminas: "; std::cin >> egz;
+       galut[i] = 0.4 * avg(nd) + 0.6 *egz;
+    }
+}
 std::cout << std::left << std::setw(15) << "Pavarde"
           << std::setw(15) << "Vardas"
           << std::right << std::setw(16) << "Galutinis(Vid)"

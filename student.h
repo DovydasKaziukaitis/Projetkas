b93f303211fde53e0135563e3dd4073ad89c83e5
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-23 03:42:56 +0200

    Enhance Student struct with ID vector and exam count
    Added a vector to store student IDs and an integer for exam count.

--- student.h
+++ student.h
@@ -4,11 +4,14 @@
 struct Student {
     std::string pav;
     std::string var;
+    std::vector<int> nd;
+    int egz = 0;
     double galVid = 0;
     double galMed = 0;
 };
 
 enum class Mode { Vid = 1, Med = 2 };
+
 
 
 

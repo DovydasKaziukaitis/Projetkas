c6e25da1f60c86c3a0de143a4867f3f808cba2ab
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-24 22:10:10 +0200

    Update io.h

--- io.h
+++ io.h
@@ -7,3 +7,4 @@ std::vector<Student> skaityti_studentus(const std::string& path);
 void spausdinti_lentele(const std::vector<Student>& s, Mode m);
 void spausdinti_lentele_abus(const std::vector<Student>& s);
 void padalinti_ir_irasyti(std::vector<Student>& s, Mode m);
+std::vector<Student> skaityti_rezultatus(const std::string& path);

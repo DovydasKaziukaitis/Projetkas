952fd99ccd59ef778a46067f6072ea6997acc5a0
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-24 23:24:41 +0200

    Update io1.h

--- io1.h
+++ io1.h
@@ -1,13 +1,12 @@
 #pragma once
-#include <string>
 #include <vector>
+#include <string>
+#include "student.h"
 #include <list>
-#include "student1.h"
 
-std::vector<Student> studentuSkaitymas(const std::string& path);
-void lentelesSpausdinimas(const std::vector<Student>& s, Mode m);
-void lentelesSpausdinimas(const std::list<Student>&    s, Mode m);
-
-void padalinimasIrIrasymas(const std::vector<Student>& s, Mode m);
-void padalinimasIrIrasymas(const std::list<Student>&    s, Mode m);
-
+std::vector<Student> skaityti_studentus(const std::string& path);
+void spausdinti_lentele(const std::vector<Student>& s, Mode m);
+void spausdinti_lentele_abus(const std::vector<Student>& s);
+void padalinti_ir_irasyti(std::vector<Student>& s, Mode m);
+void padalinti_ir_irasyti(std::list<Student>& s, Mode m);
+std::vector<Student> skaityti_rezultatus(const std::string& path);

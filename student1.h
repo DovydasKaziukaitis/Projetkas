e3aacd18777b6204f61a79691095c5a558ee4580
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-26 00:38:31 +0200

    Update student1.h

--- student1.h
+++ student1.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <string>
+#include <vector>
 
 struct Student {
     std::string pav;
     std::string var;
+    std::vector<int> nd;
+    int egz = 0;
     double galVid = 0;
     double galMed = 0;
 };
 
 enum class Mode { Vid = 1, Med = 2 };
+
 

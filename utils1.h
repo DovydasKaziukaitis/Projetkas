da5c99c6964fcfb82d34f007388f6b8d9e6c9a1d
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-26 00:39:42 +0200

    Update utils1.h

--- utils1.h
+++ utils1.h
@@ -1,7 +1,24 @@
 #pragma once
 #include <vector>
-#include <list>
 #include <string>
-#include "student1.h"
+#include "student3.h"
+#include <list>
 
-std::vector<Student> studentuGeneravimas(int m);
+std::vector<Student> generuoti_studentus(int m);
+void irasyti_pilna_faila(const std::string& path, const std::vector<Student>& s);
+void rasyti_grupe(const std::string& path, const std::vector<Student>& s, Mode m);
+
+void padalinimasStrategija1(const std::vector<Student>& s, Mode m,
+                            std::vector<Student>& varg, std::vector<Student>& kiet);
+void padalinimasStrategija1(const std::list<Student>& s, Mode m,
+                            std::list<Student>& varg, std::list<Student>& kiet);
+
+void padalinimasStrategija2(std::vector<Student>& s, Mode m,
+                            std::vector<Student>& varg, std::vector<Student>& kiet);
+void padalinimasStrategija2(std::list<Student>& s, Mode m,
+                            std::list<Student>& varg, std::list<Student>& kiet);
+
+void padalinimasStrategija3(const std::vector<Student>& s, Mode m,
+                            std::vector<Student>& varg, std::vector<Student>& kiet);
+void padalinimasStrategija3(std::list<Student>& s, Mode m,
+                            std::list<Student>& varg, std::list<Student>& kiet);

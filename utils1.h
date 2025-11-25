4eee1cc00d002e6e1e462f1e0e67e9b89ed408bb
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-25 18:54:42 +0200

    Update utils1.h

--- utils1.h
+++ utils1.h
@@ -1,10 +1,8 @@
 #pragma once
 #include <vector>
-#include <list>
 #include <string>
 #include "student1.h"
 
-std::vector<Student> studentuGeneravimas(int m);
-
-void grupesIrasymas(const std::string& path, const std::vector<Student>& s, Mode m);
-void grupesIrasymas(const std::string& path, const std::list<Student>&    s, Mode m);
+std::vector<Student> generuoti_studentus(int m);
+void irasyti_pilna_faila(const std::string& path, const std::vector<Student>& s);
+void rasyti_grupe(const std::string& path, const std::vector<Student>& s, Mode m);
